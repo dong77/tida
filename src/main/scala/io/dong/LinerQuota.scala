0a0d4c80f0db0f62d6ec6eec73ddc62bb8d88df5
wangdong <wangdong@zhongan.io>
2017-05-02 13:06:44 +0800

    add LinerQuota and HalflifeQuota

--- src/main/scala/io/dong/LinerQuota.scala
+++ src/main/scala/io/dong/LinerQuota.scala
@@ -6,14 +6,25 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import redis.protocol.Integer
 
-class LinerQuota(maxQuota: Long, spread: Duration)(
+class Quota(maxQuota: Long, decayer: Decayer)(
     implicit
-    ec: ExecutionContext, redis: RedisClient
+    ec: ExecutionContext
 ) {
+  private def toQuota(v: Long) = Math.min(Math.max(0, maxQuota - v), maxQuota)
 
-  private val decayer: Decayer = new LinerDecayer(spread)
-
-  def addQuota(key: String, quota: Long) = decayer.add(key, -quota)
-  def removeQuota(key: String, quota: Long) = decayer.add(key, quota)
-  def getQuota(key: String): Future[Long] = decayer.get(key).map { v => Math.max(0, maxQuota - v) }
+  def addQuota(key: String, quota: Long) = decayer.add(key, -quota).map(toQuota)
+  def removeQuota(key: String, quota: Long) = decayer.add(key, quota).map(toQuota)
+  def getQuota(key: String): Future[Long] = decayer.get(key).map(toQuota)
+  def removeQuotaIfSuffcient(key: String, quota: Long) = decayer.addIfLessThan(key, quota, maxQuota - quota).map(_ != -1)
 }
+
+class LinerQuota(maxQuota: Long, spread: Duration)(
+  implicit
+  ec: ExecutionContext, redis: RedisClient
+) extends Quota(maxQuota, new LinerDecayer(spread))
+
+class HalflifeQuota(maxQuota: Long, halflife: Duration)(
+  implicit
+  ec: ExecutionContext, redis: RedisClient
+) extends Quota(maxQuota, new HalflifeDecayer(halflife))
+
